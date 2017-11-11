c0c98adbd5f57d7655bac8f69e93908cd82e5ef8
walkerwaqi <srrain@outlook.com>
2017-11-11 14:00:39 +0800

    update build script

--- build-all-arch.sh
+++ build-all-arch.sh
@@ -55,10 +55,10 @@ for arch in ${archs[@]}; do
     echo "CROSS COMPILE ENV : $CROSS_COMPILE"
     cd openssl-1.0.1g
 
-    xCFLAGS="-DSHARED_EXTENSION=.so -fPIC -DOPENSSL_PIC -DDSO_DLFCN -DHAVE_DLFCN_H -mandroid -I$ANDROID_DEV/include -B$ANDROID_DEV/$xLIB -O3 -fomit-frame-pointer -Wall"
+    xCFLAGS="-DSHA512_ASM -DSHARED_EXTENSION=.so -fPIC -DOPENSSL_PIC -DDSO_DLFCN -DHAVE_DLFCN_H -mandroid -I$ANDROID_DEV/include -B$ANDROID_DEV/$xLIB -O3 -fomit-frame-pointer -Wall"
 
     perl -pi -e 's/install: all install_docs install_sw/install: install_docs install_sw/g' Makefile.org
-    ./Configure shared no-asm no-zlib no-ssl2 no-ssl3 no-comp no-hw no-engine --prefix=`pwd`/../openssl/${arch} --openssldir=`pwd`/../openssl/${arch}/ssl $configure_platform $xCFLAGS
+    ./Configure shared no-zlib no-ssl2 no-ssl3 no-comp no-hw no-engine --prefix=`pwd`/../openssl/${arch} --openssldir=`pwd`/../openssl/${arch}/ssl $configure_platform $xCFLAGS
 
     # patch SONAME
 
